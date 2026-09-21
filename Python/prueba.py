d5b650b2d05044097dfa18a334a19742fa6535b5
UNAIPESCADOR\unaip <pescadorbenito.unai@jgvaldemora.org>
2026-09-21 12:14:35 +0200

    Reesttructuracion de carpetas y creacion de un archivo de prueba en Python

---
+++ Python/prueba.py
@@ -0,0 +1,11 @@
+nombre = "unai"
+apellido = "pescador"
+print(nombre, apellido)
+edad = 20
+print("tiene", edad, "años")
+altura = 1.75
+print("mide", altura, "metros")
+
+precio = 19.99
+cantidad = "pedro"
+print("El total es:", precio * cantidad)
