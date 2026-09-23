6ddb372bc04c634f6f3b433a9d944eb70df0bb38
UNAIPESCADOR\unaip <pescadorbenito.unai@jgvaldemora.org>
2026-09-23 14:15:35 +0200

    Nueva bateria de ejercicios de Python. Se han añadido dos nuevos archivos: ejercicio4.py y ejercicio5.py. En ejercicio4.py, se solicita al usuario que ingrese su peso y altura, y luego se imprimen esos valores. En ejercicio5.py, se verifica el tipo de una variable y se imprime un mensaje indicando si es un número entero o una cadena de texto.

---
+++ Python/Ejercicios Python/ejercicio4.py
@@ -0,0 +1,5 @@
+peso = float(input("Ingrese su peso en kilogramos: "))
+altura = float(input("Ingrese su altura en metros: "))
+
+print("Su peso es:", peso, "kg")
+print("Su altura es:", altura, "m")
---
+++ Python/Ejercicios Python/ejercicio5.py
@@ -0,0 +1,7 @@
+var = "hola"
+
+if type(var) == int:
+    print("La variable es un número entero")
+else:
+    print("La variable es una cadena de texto")
+    
